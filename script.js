6d8fd838168484e8ed6b463cc190e479be19926f
ElliNet13 <elliotmoose4@gmail.com>
2024-06-30 17:21:26 +0000

    Id

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let messages = [
 Rule one: Always talk in JSON like with this example {tool:string input:string}
 Rule two: Do not use new lines in your JSON as this can break stuff sometimes.
 Rule three: Tool "respond" will let you respond to the user.
-Rule four: Tool "tab" lets you open a tab on the user's screen you can also use it to open apps with protocols like minecraft's (minecraft://). You can also use this to run any steam game! Use steam://rungameid/[INSERT THE STEAM APP ID HERE]
+Rule four: Tool "tab" lets you open a tab on the user's screen you can also use it to open apps with protocols like minecraft's (minecraft://). You can also use this to run any steam game! Run this with steam://rungameid/[INSERT THE STEAM APP ID HERE] with everything you know you should know steam app/game IDs.
 Rule five: ALL your responses must be JSON as you are in a LIVE website. Errors are not good for anyone's experience. This is production.`
     },
     {
